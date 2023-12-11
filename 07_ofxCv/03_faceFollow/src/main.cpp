815c88cbf59cbd409d33b97328b0d88fc313c150
Atsushi Tadokoro <tadokoro@gmail.com>
2023-12-11 14:04:27 +0900

    update main.cpp

--- 07_ofxCv/03_faceFollow/src/main.cpp
+++ 07_ofxCv/03_faceFollow/src/main.cpp
@@ -1,6 +1,17 @@
+#include "ofMain.h"
 #include "ofApp.h"
 
-int main() {
-	ofSetupOpenGL(640, 480, OF_WINDOW);
-	ofRunApp(new ofApp());
+//========================================================================
+int main( ){
+
+	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+	ofGLWindowSettings settings;
+	settings.setSize(1024, 768);
+	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+
+	auto window = ofCreateWindow(settings);
+
+	ofRunApp(window, make_shared<ofApp>());
+	ofRunMainLoop();
+
 }
