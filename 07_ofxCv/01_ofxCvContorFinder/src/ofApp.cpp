b704cd6978faabf05d229dde066b0cb2b6bf8428
Atsushi Tadokoro <tadokoro@gmail.com>
2023-12-11 16:12:22 +0900

    Update ofApp.cpp

--- 07_ofxCv/01_ofxCvContorFinder/src/ofApp.cpp
+++ 07_ofxCv/01_ofxCvContorFinder/src/ofApp.cpp
@@ -5,14 +5,15 @@ using namespace cv;
 
 void ofApp::setup() {
     cam.initGrabber(ofGetWidth(), ofGetHeight());
-    contourFinder.setMinAreaRadius(10);
+    contourFinder.setMinAreaRadius(50);
     contourFinder.setMaxAreaRadius(200);
 }
 
 void ofApp::update() {
     cam.update();
-    if(cam.isFrameNew()) {
-        contourFinder.setThreshold(ofMap(mouseX, 0, ofGetWidth(), 0, 255));
+    if (cam.isFrameNew()) {
+        float thresh = ofMap(mouseX, 0, ofGetWidth(), 0, 255);
+        contourFinder.setThreshold(thresh);
         contourFinder.findContours(cam);
     }
 }
@@ -21,4 +22,9 @@ void ofApp::draw() {
     ofSetColor(255);
     cam.draw(0, 0);
     contourFinder.draw();
+    for (int i = 0; i < contourFinder.size(); i++) {
+        cv::Point2f center = contourFinder.getCenter(i);
+        ofSetColor(255, 0, 0);
+        ofDrawCircle(center.x, center.y, 5);
+    }
 }
